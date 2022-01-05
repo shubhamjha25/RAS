3f8b2133185406c91e674d7774dd0c6b3f2f5a0f
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-05 23:47:01 +0530

    view menu items (admin)

--- src/components/ManageItems.js
+++ src/components/ManageItems.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "../App.css";
 
 const ManageItems = () => {
+
+    var isAuth = false;
+    var token = localStorage.getItem('tokenStore');
+    if(token)
+        isAuth = true;
+
+    const [items, setItems] = useState([]);
+
+    const getItems = async () => {
+        const res = await axios.get('https://ras-api-server.herokuapp.com/api/items');
+        console.log(res.data)
+        setItems(res.data);
+    }
+
+    useEffect(() => {
+        getItems();
+    }, []);
+
+
     return (
-        <h1>Manage Items Page</h1>
+        <div className='manage-items-page'>
+            <h1>Menu Items</h1><br />
+            <h3>Here are the available items</h3><br />
+            <div className='menu-items-container'>
+                {
+                    items.map( (item) => {
+                        return (
+                            <div className='item-card'>
+                                <img className='item-card-img' src={item.img} alt="food-avatar" />
+                                <h3>{item.title}</h3>
+                                <h4>{item.desc}</h4>
+                                <br />
+                                <h4>INR <strong>{item.price}</strong></h4><br />
+                                <div className='item-btn-container'>
+                                    <a className='item-btn-edit'>EDIT</a>
+                                    <a className='item-btn-dlt'>DELETE</a>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </div>
+
     );
 }
 
