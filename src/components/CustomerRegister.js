ef5b1ed258b7f95a7e8ecb410a9277bb795a410a
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-06 16:37:10 +0530

    customer registration

--- src/components/CustomerRegister.js
+++ src/components/CustomerRegister.js
@@ -1,8 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import '../App.css';
 
 const CustomerRegister = () => {
+
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const [err, setErr] = useState('');
+
+    const navigate = useNavigate();
+
+    const onChangeInput = e => {
+        const { name, value } = e.target;
+        setUser({...user, [name]:value});
+        setErr('');
+    }
+
+    const registerSubmit = async e =>{
+        e.preventDefault()
+        try {
+            const res = await axios.post('https://ras-api-server.herokuapp.com/api/auth/register',{
+                username: user.username,
+                email: user.email,
+                password: user.password
+            });
+            setUser({username: '', email: '', password: ''});
+            return navigate('/customer/login');
+        } catch (err) {
+            console.log(err);
+            setErr(err.response.data);
+        }
+    }
+
     return(
-        <h1>Customer Register Page</h1>
+        <div className='admin-login-page-container'>
+            <h1>Customer Registration</h1><br />
+
+            <h3>Welcome to RAS!</h3>
+            <h4>First things first. Lets create a new account and get started</h4>
+
+            <br /><br />
+
+            <form onSubmit={registerSubmit} method="post" className = "log-form">
+                <div className="group log-input">
+                    <input type="text" id = "username" name = "username" placeholder = "Username" required value={user.username} onChange={onChangeInput} />
+                </div>
+
+                <br />
+
+                <div className="group log-input">
+                    <input type="email" id = "email" name = "email"  placeholder = "E-Mail" required value={user.email} onChange={onChangeInput} />
+                </div>
+
+                <br />
+
+                <div className="group log-input">
+                    <input type="password" id = "password" name = "password"  placeholder = "Password" required value={user.password} autoComplete="true" onChange={onChangeInput} />
+                </div>
+
+                <br />
+
+                {
+                    err ? <><p className='log-form-err'>{err}</p><br /></> : <br />
+                }
+                
+                <div className="container-log-btn">
+                    <button type="submit" name = "btn_submit" className="log-form-btn">
+                    <span>REGISTER</span>
+                    </button>
+                </div>
+            </form>
+            <br />
+            <h3>Already Have an Account? <br /><br /> <Link className='customer-auth-btn' to="/customer/login">LOGIN HERE</Link></h3>
+        </div>
     );
 }
 
