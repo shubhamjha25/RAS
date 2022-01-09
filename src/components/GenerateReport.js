b7146c3c7528062c62359da1a7b904df7bdadd9f
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-10 02:23:01 +0530

    generate sales report

--- src/components/GenerateReport.js
+++ src/components/GenerateReport.js
@@ -9,7 +9,14 @@ const GenerateReport = () => {
     const [totalOrder, setTotalOrder] = useState(0);
     const [currentIncome, setCurrentIncome] = useState(0);
     const [prevIncome, setPrevIncome] = useState(0);
-    const [percentage, setPercentage] = useState(0);
+
+    const date = new Date();
+    var currentMonth = date.getMonth()+1;
+    var prevMonth;
+    if(currentMonth === 1)
+        prevMonth = 12;
+    else
+        prevMonth = currentMonth - 1;
 
     var isAuth = false;
     var token = localStorage.getItem('tokenStore');
@@ -33,50 +40,43 @@ const GenerateReport = () => {
     }
 
     const getSales = async () => {
-        const res = await axios.get('http://localhost:5000/api/orders/income',
-            {headers: {token: token}},
+        const res = await axios.get('https://ras-api-server.herokuapp.com/api/orders/income',
+            {headers: {token: token}}, 
             );
-            setSales(res.data);
-            console.log(res.data);
-    }
-
-    const calculateSales = () => {
-        const date = new Date();
-        let currentMonth = date.getMonth()+1;
-        var prevMonth;
-        if(currentMonth === 1)
-            prevMonth = 12;
-        else
-            prevMonth = currentMonth - 1;
-
-        sales.map(sale => {
-            if(sale._id === currentMonth)
-                setCurrentIncome(sale.total);
-            if(sale._id === prevMonth)
-                setPrevIncome(sale.total)
-        });
-    }
-
-    const calculatePercentage = () => {
-        setPercentage(20);
+        sales.push(res.data);
+        console.log(currentMonth, ' & ', prevMonth)
+        console.log(sales)
+        console.log(sales.length);
+        for(let i=0; i<sales[0].length; i++) {
+            if(sales[0][i]._id === currentMonth) {
+                setCurrentIncome((currentIncome) => currentIncome+sales[0][i].total);
+            }
+            if(sales[0][i]._id === prevMonth) {
+                setPrevIncome((prevIncome) => prevIncome+sales[0][i].total);
+            }
+        }
+        console.log(currentIncome, ' & ', prevIncome)       
     }
     
-
     useEffect(() => {
         getOrders();
         getSales();
-        calculateSales();
-        calculatePercentage();
     }, []);
 
     return (
         <div className='reports-page'>
-            <h1>Sales Report</h1><br /><br />
-            <h3>Total Income Generated (Lifetime) : { totalIncome() }</h3><br />
+            <h1>Sales Report</h1><hr /><br /><br />
+            <h3>Total Income Generated (Lifetime) : INR { totalIncome() }</h3><br />
             <h3>Total Number of Orders : {totalOrder}</h3><br />
-            <h3>Income Generated in the Current Month = {currentIncome}</h3><br />
-            <h3>Income Generated in the Last Month = {prevIncome}</h3><br />
-            <h3>Sales Percentage = {percentage}%</h3>
+            <h3>Income Generated in the Current Month = INR  { currentIncome }</h3><br />
+            <h3>Income Generated in the Last Month = INR { prevIncome }</h3><br />
+            {
+                parseFloat(((currentIncome-prevIncome)/prevIncome)*100).toFixed(2)>0 
+                    ?
+                        <p className='positive-sales'><strong>+ {parseFloat(((currentIncome-prevIncome)/prevIncome)*100).toFixed(2)} %&emsp;Sales Growth</strong></p> 
+                    :
+                        <p className='negative-sales'><strong>- {parseFloat(((currentIncome-prevIncome)/prevIncome)*100).toFixed(2)} %&emsp;Sales Decline</strong></p>
+            }
         </div>
     );
 }
