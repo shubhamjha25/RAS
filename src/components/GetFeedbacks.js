2794d8da1dd426a942752ee967ad810110e98420
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-10 23:25:04 +0530

    get feedbacks page

--- src/components/GetFeedbacks.js
+++ src/components/GetFeedbacks.js
@@ -4,50 +4,48 @@ import '../App.css';
 
 const GetFeedbacks = () => {
 
-    const [orders, setOrders] = useState([]);
-    const [totalOrder, setTotalOrder] = useState(0);
+    const [feedbacks, setFeedbacks] = useState([]);
+    const [totalFeedbacks, setTotalFeedbacks] = useState(0);
 
     var isAuth = false;
     var token = localStorage.getItem('tokenStore');
     if(token)
         isAuth = true;
 
-    const getOrders = async () => {
-        const res = await axios.get('https://ras-api-server.herokuapp.com/api/orders/',
+    const getFeedbacks = async () => {
+        const res = await axios.get('https://ras-api-server.herokuapp.com/api/feedbacks/all/',
             {headers: {token: token}}, 
             );
-        setOrders(res.data);
-        setTotalOrder(res.data.length);
+        setFeedbacks(res.data);
+        setTotalFeedbacks(res.data.length);
     }
 
     useEffect(() => {
-        getOrders();
+        getFeedbacks();
     }, []);
 
     return (
         <div className='reports-page'>
-            <h1>Orders</h1><hr /><br />
-            <h3>Total Number of Orders (Lifetime): {totalOrder}</h3><br />
-            <h3>List of Orders</h3><br />
+            <h1>User Feedbacks</h1><hr /><br />
+            <h3>Total Number of Feedbacks Recieved : {totalFeedbacks}</h3><br />
+            <h3>List of Feedbacks</h3><br />
             <table className='list-of-orders'>
                 <tr>
                     <th className='order-table-heading'>S. No.</th>
-                    <th className='order-table-heading'>Order Id</th>
-                    <th className='order-table-heading'>Ordered By</th>
-                    <th className='order-table-heading'>Date</th>
-                    <th className='order-table-heading'>Amount</th>
-                    <th className='order-table-heading'>Status</th>
+                    <th className='order-table-heading'>Feedback ID</th>
+                    <th className='order-table-heading'>Given By</th>
+                    <th className='order-table-heading'>Feedback Message</th>
+                    <th className='order-table-heading'>Rating <br /> (Out of 5)</th>
                 </tr>
                 {
-                orders.map((order, key) => {
+                feedbacks.map((feedback, key) => {
                     return (
                         <tr>
                             <td className='order-table-values'>{key+1}</td>
-                            <td className='order-table-values'>{order._id}</td>
-                            <td className='order-table-values'>{order.username}</td>
-                            <td className='order-table-values'>{order.createdAt}</td>
-                            <td className='order-table-values'>{order.amount}</td>
-                            <td className='order-table-values'>{order.status}</td>
+                            <td className='order-table-values'>{feedback._id}</td>
+                            <td className='order-table-values'>{feedback.username}</td>
+                            <td className='order-table-values'>{feedback.message}</td>
+                            <td className='order-table-values'>{feedback.rating}</td>
                         </tr>
                     )
                     
