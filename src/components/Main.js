566e0a6e74b830948deddceae9b96c887a389860
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-13 23:13:23 +0530

    user-cards img updated & Staff card added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LOGO from '../logo.png';
 
 const Card = (props) => {
     return (
@@ -13,14 +14,14 @@ const Card = (props) => {
 const Main = () => {
     return (
         <div className='main'>
-            <br /><br />
+            <img src={LOGO} className='ras-logo' />
             <h1>Restaurant Automation System (RAS)</h1>
             <br />
             <h3>(Select User Type)</h3>
-            <br /> <br />
             <div className='main-card-container'>
-                <Link to="/customer/login"><Card userType="Customer" img="https://cdn3.iconfinder.com/data/icons/business-finance-line-5/32/client-2-512.png" /></Link>
-                <Link to="/admin"><Card userType="Admin" img="https://cdn2.iconfinder.com/data/icons/mobile-banking-ver-1a/100/1-11-512.png" /></Link>
+                <Link to="/customer/login"><Card userType="Customer" img="https://www.clipartmax.com/png/full/86-865575_dinner-eating-food-lunch-meal-people-restaurant-eating-dinner-icon-png.png" /></Link>
+                <Link to="/staff/login"><Card userType="Staff" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjAY8pdueayjQaeRs-d5oAGkNZGD9YtaFNyg&usqp=CAU" /></Link>
+                <Link to="/admin"><Card userType="Admin" img="https://cdn-icons-png.flaticon.com/512/2206/2206368.png" /></Link>
             </div>
         </div>
     );
