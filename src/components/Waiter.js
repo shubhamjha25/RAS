d8203fef195efd6075302906a7fcd22626833642
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-14 21:57:06 +0530

    staff pages

---
+++ src/components/Waiter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Waiter = () => {
+    
+    const logoutSubmit = () => {
+        localStorage.clear();
+    }
+    
+    return (
+        <>
+            <h1>Waiter Home Page</h1>
+            <Link to="/"><button onClick={logoutSubmit} style={{padding: "10px 25px", marginBottom: "20px", fontSize: "1.25rem", fontWeight: "bold", borderRadius: "25px", cursor: "pointer"}}>CLICK TO LOGOUT</button></Link>
+        </>
+    )
+}
+
+export default Waiter;
