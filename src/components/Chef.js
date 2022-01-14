0b5d7fa3655727f53c4863fab8250f44e9882e1f
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-14 23:22:40 +0530

    chef page design

--- src/components/Chef.js
+++ src/components/Chef.js
@@ -1,16 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
 
 const Chef = () => {
-    
+
+    const [orders, setOrders] = useState([]);
+    const [totalOrder, setTotalOrder] = useState(0);
+
+    var isAuth = false;
+    var token = localStorage.getItem('tokenStore');
+    if(token)
+        isAuth = true;
+
+    if(isAuth == true)
+    {
+        var decoded = jwt_decode(token);
+        var id = decoded.id;
+        var username = decoded.name;
+    }
+
     const logoutSubmit = () => {
         localStorage.clear();
     }
+
+    const getOrders = async () => {
+        const res = await axios.get(`https://ras-api-server.herokuapp.com/api/orders/getOrders`,
+            {headers: {token: token}}, 
+            );
+        setOrders(res.data);
+        if(res.data !== null) {
+            setTotalOrder(res.data.length);
+        }
+    }
     
+    useEffect(() => {
+        getOrders();
+    }, []);
+
     return (
         <>
-            <h1>Chef Home Page</h1>
-            <Link to="/"><button onClick={logoutSubmit} style={{padding: "10px 25px", marginBottom: "20px", fontSize: "1.25rem", fontWeight: "bold", borderRadius: "25px", cursor: "pointer"}}>CLICK TO LOGOUT</button></Link>
+            <div className="customer-home-container">
+                <h1>Chef Dashboard</h1><br />
+                <Link to="/"><button onClick={logoutSubmit} style={{padding: "10px 25px", marginBottom: "20px", fontSize: "1.25rem", fontWeight: "bold", borderRadius: "25px", cursor: "pointer"}}>CLICK TO LOGOUT</button></Link>
+                <br /><br />
+                {
+                    totalOrder
+                        ?
+                            <>
+                                <table className='order-table'>
+                                    <tr>
+                                        <th className='order-table-heading'>S. No.</th>
+                                        <th className='order-table-heading'>Order Id</th>
+                                        <th className='order-table-heading'>Order Details</th>
+                                        <th className='order-table-heading'>Ordered By</th>
+                                        <th className='order-table-heading'>Date</th>
+                                        <th className='order-table-heading'>Amount (INR)</th>
+                                        <th className='order-table-heading'>Status</th>
+                                        <th className='order-table-heading'>Action</th>
+                                    </tr>
+                                    {
+                                    orders.map((order, key) => {
+                                        return (
+                                            <tr>
+                                                <td className='order-table-values'>{key+1}</td>
+                                                <td className='order-table-values'>{order._id}</td>
+                                                <td className='order-table-values'>
+                                                    {
+                                                        order.items.map(orderItem => {
+                                                            return (
+                                                                <p>{orderItem.quantity} {orderItem.title}</p>
+                                                            )
+                                                        })
+                                                    }
+                                                </td>
+                                                <td className='order-table-values'>{order.username}</td>
+                                                <td className='order-table-values'>{order.createdAt}</td>
+                                                <td className='order-table-values'>{order.amount}</td>
+                                                {
+                                                    order.status === 'pending'
+                                                        ?
+                                                            <td className='order-table-values'><strong className='pending-btn'>{order.status}</strong></td>
+                                                        :
+                                                            order.status === 'completed'
+                                                                ?
+                                                                    <td className='order-table-values'><strong className='completed-btn'>{order.status}</strong></td>
+                                                                :
+                                                                    <td className='order-table-values'><strong className='prepared-btn'>{order.status}</strong></td>
+                                                }
+                                                {
+                                                    order.status === "pending"
+                                                        ?
+                                                            <td className='order-table-values'><button className='mark-as-prepared-btn'>MARK AS PREPARED</button></td>
+                                                        :
+                                                            <td className='order-table-values'><strong>N/A</strong></td>
+                                                }
+                                            </tr>
+                                        )
+                                        
+                                    })}
+                                </table>
+                            </>
+                        :
+                            <h2>No Orders Found</h2>
+                }
+            </div>
         </>
     )
 }
