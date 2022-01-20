60a7fb07d0a51cd15b23177bf1ea6be986e56e0c
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-21 00:21:55 +0530

    admin invoice generation

---
+++ src/components/AdminGenBill.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PrintBill from './PrintBill';
+
+const AdminGenBill = () => {
+    return (
+        <>
+            <PrintBill />
+        </>
+    )
+}
+
+export default AdminGenBill;
