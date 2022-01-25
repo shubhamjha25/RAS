cdc05fa832e30c7f3bf404916a0e7d7a6c223c71
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-25 16:14:28 +0530

    restrict user access by protecting routes

--- src/App.js
+++ src/App.js
@@ -26,33 +26,91 @@ import Waiter from './components/Waiter';
 import PrintBill from './components/PrintBill';
 import AdminInvoices from './components/AdminInvoices';
 import AdminGenBill from './components/AdminGenBill';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
     return (
         <div className="App">
             <Routes>
+                {/*PUBLIC ROUTES*/}
                 <Route path="/" element={<Main />} />
                 <Route path="/customer/login" element={<CustomerLogin />} />
                 <Route path="/customer/register" element={<CustomerRegister />} />
-                <Route path="/customer/home" element={<CustomerHome />} />
-                <Route path="/customer/orders" element={<CustomerOrders />} />
-                <Route path="/customer/cart" element={<CustomerCart />} />
-                <Route path="/customer/orderSuccess" element={<OrderSuccess />} />
                 <Route path="/admin" element={<AdminPage />} />
-                <Route path="/admin/home" element={<AdminHome />} />
-                <Route path="/admin/users" element={<GetUsers />} />
-                <Route path="/admin/report" element={<GenerateReport />} />
-                <Route path="/admin/orders" element={<ViewOrders />} />
-                <Route path="/admin/feedbacks" element={<GetFeedbacks />} />
-                <Route path='/admin/invoices' element={<AdminInvoices />} />
-                <Route path='/admin/invoices/:id' element={<AdminGenBill />} />
-                <Route path="/admin/items" element={<ManageItems />} />
-                <Route path="/admin/items/add" element={<AddMenuItems />} />
-                <Route path="/admin/items/edit/:id" element={<EditItem />} />
                 <Route path="/staff/login" element={<StaffLogin />} />
-                <Route path="/staff/chef/:id" element={<Chef />} />
-                <Route path="/staff/waiter/:id" element={<Waiter />} />
-                <Route path="/staff/waiter/:id/bill/:billId" element={<PrintBill />} />
+
+                {/*CUSTOMER PRIVATE ROUTES*/}
+                <Route exact path="/customer/home" element={<PrivateRoute />}>
+                    <Route exact path="/customer/home" element={<CustomerHome/>}/>
+                </Route>
+
+                <Route exact path="/customer/orders" element={<PrivateRoute />}>
+                    <Route path="/customer/orders" element={<CustomerOrders />} />
+                </Route>
+
+                <Route exact path="/customer/cart" element={<PrivateRoute />}>
+                    <Route path="/customer/cart" element={<CustomerCart />} />
+                </Route>
+
+                <Route exact path="/customer/orderSuccess" element={<PrivateRoute />}>
+                    <Route path="/customer/orderSuccess" element={<OrderSuccess />} />
+                </Route>
+                
+                {/*ADMIN PRIVATE ROUTES*/}
+                <Route exact path="/admin/home" element={<PrivateRoute />}>
+                    <Route path="/admin/home" element={<AdminHome />} />
+                </Route>
+
+                <Route exact path="/admin/users" element={<PrivateRoute />}>
+                    <Route path="/admin/users" element={<GetUsers />} />
+                </Route>
+
+                <Route exact path="/admin/report" element={<PrivateRoute />}>
+                    <Route path="/admin/report" element={<GenerateReport />} />
+                </Route>
+
+                <Route exact path="/admin/orders" element={<PrivateRoute />}>
+                    <Route path="/admin/orders" element={<ViewOrders />} />
+                </Route>
+
+                <Route exact path="/admin/feedbacks" element={<PrivateRoute />}>
+                    <Route path="/admin/feedbacks" element={<GetFeedbacks />} />
+                </Route>
+
+                <Route exact path="/admin/invoices" element={<PrivateRoute />}>
+                    <Route path='/admin/invoices' element={<AdminInvoices />} />
+                </Route>
+
+                <Route exact path="/admin/invoices/:id" element={<PrivateRoute />}>
+                    <Route path="/admin/invoices/:id" element={<AdminGenBill />} />
+                </Route>
+
+                <Route exact path="/admin/items" element={<PrivateRoute />}>
+                    <Route path="/admin/items" element={<ManageItems />} />
+                </Route>
+
+                <Route exact path="/admin/items/add" element={<PrivateRoute />}>
+                    <Route path="/admin/items/add" element={<AddMenuItems />} />
+                </Route>
+
+                <Route exact path="/admin/items/edit/:id" element={<PrivateRoute />}>
+                    <Route path="/admin/items/edit/:id" element={<EditItem />} />
+                </Route>
+
+                {/*STAFF PRIVATE ROUTES*/}
+                <Route exact path="/staff/chef/:id" element={<PrivateRoute />}>
+                    <Route path="/staff/chef/:id" element={<Chef />} />
+                </Route>
+
+                <Route exact path="/staff/waiter/:id" element={<PrivateRoute />}>
+                    <Route path="/staff/waiter/:id" element={<Waiter />} />
+                </Route>
+
+                <Route exact path="/staff/waiter/:id/bill/:billId" element={<PrivateRoute />}>
+                    <Route path="/staff/waiter/:id/bill/:billId" element={<PrintBill />} />
+                </Route>
+    
+                {/*404 Error Handler*/}
                 <Route path="*" element={<Page404 />} />
             </Routes>
         </div>
