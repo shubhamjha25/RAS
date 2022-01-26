631b9f55ceea0e93ecfaa45a17df3d91e32e5feb
Shubham Jha <shubham.jha2001m@gmail.com>
2022-01-26 20:36:13 +0530

    fix url in 404 page

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Page404 = () => {
-
+    
     const style = {
         display: "flex",
         flexDirection: "column",
@@ -18,7 +18,7 @@ const Page404 = () => {
         <div style={style}>
             <img src="https://cdn4.iconfinder.com/data/icons/thin-gui-elements-2/24/thin-1550_website_error_missing_404-512.png" style={{width: "25%"}} alt="Please Login" /><br />
             <h1>SORRY! We are unable to find the page you are looking for. ☹️</h1><br />
-            <Link to="/RAS"><button onClick={() => clearLocalStorage()} className='log-form-btn' style={{width: "200px"}}>Go Back</button></Link>
+            <Link to="/"><button onClick={() => clearLocalStorage()} className='log-form-btn' style={{width: "200px"}}>Go Back</button></Link>
         </div>
     );
 }
